79d0ff5c8108988ea2f94e0b868cf0284dc0fb39
sudharm jadhav <93636952+sudharm2004@users.noreply.github.com>
2022-10-29 14:20:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
 console.log('Dictionary project');
 
 let searchBtn=document.getElementById('searchBtn');
-searchBtn.addEventListener("click",searchDefinition);
+searchBtn.addEventListener("click",displayDefinitons);
 
 async function fetchData(word)
 {
+    
+
+    
         let response= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-        let data=await response.json();
+        const data=await response.json();
+    console.log(data);
         return data;
 
 }
 
-function searchDefinition() {
+async function displayDefinitons() {
+
     let inputVal=document.getElementById('inputVal');
     let list=document.getElementById('list');
     let uiString="";
     let definitions="";
     let word=inputVal.value.toLowerCase();
     word=word.trim()
+
     if(word==undefined||word.length==0)
-    {
-        alert('please enter the word to search');
-    }
+        {
+            alert('please enter the word to search');
+        }
     else
-    {
-        fetchData(word).then((data)=>{
-            data.forEach(element => {
-                let meanings=element.meanings;
-                meanings.forEach(elem=>{
-                    let definitions=elem.definitions;
-                    definitions.forEach(definitionsElem=>{
-                        if (definitionsElem.example!=undefined) {
-                            uiString+=`<li class="list-group-item">${definitionsElem.definition}<br>For example:-${definitionsElem.example}</li>`
-                        } else {
-                            uiString+=`<li class="list-group-item">${definitionsElem.definition}</li>`
-                        }
-                        // console.log(uiString)
+        {
+            fetchData(word).then((data)=>{
+                if (data.title=='No Definitions Found') {
+                    uiString=`<li class="list-group-item">${data.message}</li>`;
+                    inputVal.value="";
+                    list.innerHTML=uiString;
+                } else {
+                    console.log('after',data);
+                    data.forEach(element => {
+                        let meanings=element.meanings;
+                        meanings.forEach(elem=>{
+                            let definitions=elem.definitions;
+                            definitions.forEach(definitionsElem=>{
+                                if (definitionsElem.example!=undefined) {
+                                    uiString+=`<li class="list-group-item">${definitionsElem.definition}<br>For example:-${definitionsElem.example}</li>`
+                                } else {
+                                    uiString+=`<li class="list-group-item">${definitionsElem.definition}</li>`
+                                }
+                                // console.log(uiString)
+                            });
+                        });
                     });
-                });
+                    inputVal.value="";
+                    list.innerHTML=uiString;
+                }
             });
-        }).catch(()=>alert('invalid word or word not present in dictionary'))
-    }
-    setTimeout(()=>{
-        // console.log(uiString)
-        inputVal.value="";
-        list.innerHTML=uiString;
-    },1000)
-    
+            
+                
+        }
 }
